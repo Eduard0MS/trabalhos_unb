071a7eaad7c666fc29ee8e1205c2cad4c74924d8
Eduardo Moura <eduardomsx1@gmail.com>
2023-11-18 02:35:21 -0300

    Update main.rs
    O(n)

--- rust/03_factorial/src/main.rs
+++ rust/03_factorial/src/main.rs
@@ -1,17 +1,28 @@
+use std::collections::HashMap;
 use std::ops::Sub;
 use std::fmt::Display;
 use num_traits::identities::One;
 
-fn fact<T: PartialOrd + PartialEq + One + Sub<Output = T> + Copy + Display>(x: T) -> T {
+fn fact<T>(x: T, memo: &mut HashMap<T, T>) -> T
+where
+    T: PartialOrd + PartialEq + One + Sub<Output = T> + Copy + Display,
+{
     if x < T::one() {
         panic!("Invalid number: {}", x);
     } else if x.is_one() {
         return T::one();
     }
 
-    return x * fact(x - T::one());
+    if let Some(&result) = memo.get(&x) {
+        return result;
+    }
+
+    let result = x * fact(x - T::one(), memo);
+    memo.insert(x, result);
+    result
 }
 
 fn main() {
-    println!("{}", fact(3));
+    let mut memo: HashMap<u64, u64> = HashMap::new();
+    println!("{}", fact(3u64, &mut memo));
 }
