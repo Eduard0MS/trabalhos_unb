bcc7de1e65cec8b484fdab868f219dbf91691cd2
Eduardo Moura <eduardomsx1@gmail.com>
2024-01-24 12:45:39 -0300

    Add files via upload
    Claro, aqui está um exemplo básico de um arquivo README para o projeto:
    
    ```markdown
    # Robô de Operações Binárias
    
    Este é um projeto de um robô de operações binárias desenvolvido em Python, utilizando a biblioteca IQOption API para se conectar à plataforma IQ Option.
    
    ## Configuração
    
    1. Clone o repositório:
    
       ```bash
       git clone https://github.com/seu-usuario/seu-repositorio.git
       cd seu-repositorio
       ```
    
    2. Instale as dependências:
    
       ```bash
       pip install -r requirements.txt
       ```
    
    3. Configure as informações necessárias no arquivo `config.txt`:
    
       - `LOGIN`: Insira seu e-mail e senha da IQ Option.
       - `AJUSTES`: Configure o tipo, valor de entrada, stop loss e stop win.
       - `MARTINGALE`: Configure as opções do Martingale.
       - `SOROS`: Configure as opções do Soros.
    
    4. Execute o robô:
    
       ```bash
       python nome_do_seu_arquivo.py
       ```
    
    ## Funcionalidades
    
    - **Conexão com a IQOption:** O robô se conecta à plataforma IQ Option usando suas credenciais.
    
    - **Configurações Personalizáveis:** Você pode ajustar várias configurações no arquivo `config.txt` para personalizar o comportamento do robô.
    
    - **Estratégia MHI:** O robô implementa uma estratégia de análise MHI para tomar decisões de operações binárias.
    
    - **Tratamento de Exceções:** O código possui tratamento de exceções para lidar com erros de forma apropriada.
    
    ## Contribuições
    
    Contribuições são bem-vindas! Sinta-se à vontade para abrir problemas (issues) ou enviar pull requests para melhorar este projeto.
    
    ## Autor
    
    Eduardo
    
    ## Licença
    
    Este projeto está licenciado sob a [Licença MIT](LICENSE).
    ```
    
    Lembre-se de substituir `seu-usuario/seu-repositorio` pelo nome de usuário e nome do repositório adequados em seu serviço de controle de versão. Personalize as seções conforme necessário para refletir as características específicas do seu projeto.

---
+++ boot.py
@@ -0,0 +1,115 @@
+# -*- coding: iso-8859-1 -*-
+
+
+############### YOUTUBE #####################
+from iqoptionapi.stable_api import IQ_Option
+import time
+
+
+### preencha seu email e senha aqui abaixo ###
+email = 'eduardomiuradasilva@gmail.com'
+senha = '1802Edu0#*#'
+
+API = IQ_Option(email,senha)
+
+
+### Função para conectar na IQOPTION ###
+check, reason = API.connect()
+if check:
+    print('Conectado com sucesso')
+else:
+    if reason == '{"code":"invalid_credentials","message":"You entered the wrong credentials. Please ensure that your login/password is correct."}':
+        print('Email ou senha incorreta')
+        
+    else:
+        print('Houve um problema na conexão')
+
+        print(reason)
+
+### Função para Selecionar demo ou real ###
+while True:
+    escolha = input('Selecione a conta em que deseja conectar: demo ou real  - ')
+    if escolha == 'demo':
+        conta = 'PRACTICE'
+        print('Conta demo selecionada')
+        break
+    if escolha == 'real':
+        conta = 'REAL'
+        print('Conta real selecionada')
+        break
+    else:
+        print('Escolha incorreta! Digite demo ou real')
+        
+API.change_balance(conta)
+
+### Função abrir ordem e checar resultado ###
+def compra(ativo,valor,direcao,exp,tipo):
+    if tipo == 'digital':
+        check, id = API.buy_digital_spot_v2(ativo,valor,direcao,exp)
+    else:
+        check, id = API.buy(valor,ativo,direcao,exp)
+
+
+    if check:
+        print('Ordem executada ',id)
+
+        while True:
+            time.sleep(0.1)
+            status , resultado = API.check_win_digital_v2(id) if tipo == 'digital' else API.check_win_v4(id)
+
+            if status:
+                if resultado > 0:
+                    print('WIN', round(resultado,2))
+                elif resultado == 0:
+                    print('EMPATE', round(resultado,2))
+                else:
+                    print('LOSS', round(resultado,2))
+                break
+
+    else:
+        print('erro na abertura da ordem,', id)
+        
+
+#ativo = 'EURUSD'
+valor = 10.50
+direcao = 'call'
+exp = 1
+tipo = 'digital'
+
+
+### chamada da função de compra ###
+compra(ativo,valor,direcao,exp,tipo)
+
+
+
+
+
+### Função para separar tipos de dados de uma ordem ###
+def separar_dados(dados):
+    dados = dados.split(';')
+    dados = [float(i) for i in dados]
+    return dados
+
+### Função para pegar dados de uma ordem ###
+def pegar_dados_dig(id,tipo):
+    if tipo == 'digital':
+        dados = API.get_digital_spot_profit_after_sale(id)
+    else:
+        dados = API.get_option_profit_after_sale(id)
+    return dados
+def pegar_dados_bin(id,tipo):
+    if tipo == 'binaria':
+        dados = API.get_digital_spot_profit_after_sale(id)
+    else:
+        dados = API.get_option_profit_after_sale(id)
+    return dados
+
+### Função para tratamento de dados ###
+def tratamento_dados(dados):
+    dados = dados.split(';')
+    dados = [float(i) for i in dados]
+    for j in range(len(dados)):
+        if dados[j] == 0:
+            return 0
+        else:
+            return dados[j]
